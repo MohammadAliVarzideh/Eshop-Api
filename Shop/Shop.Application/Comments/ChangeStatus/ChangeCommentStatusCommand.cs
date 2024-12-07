e956ed3a6ad6ceedc62e9d93e3aa902e817773c1
Mohammad <mohammadalivarzideh2@gmail.com>
2024-12-07 17:20:43 +0330

    order

---
+++ Shop/Shop.Application/Comments/ChangeStatus/ChangeCommentStatusCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Application;
+using Shop.Domain.CommentAgg;
+
+namespace Shop.Application.Comments.ChangeStatus
+{
+    public record ChangeCommentStatusCommand(long Id, CommentStatus status):IBaseCommand;
+
+}
